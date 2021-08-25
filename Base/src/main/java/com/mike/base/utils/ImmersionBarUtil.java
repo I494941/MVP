c74960eacb1f614b8d0c38357bfbe52a6d27bd67
I494941 <865494249@qq.com>
2021-08-25 13:53:53 +0800

    代码首次提交

---
+++ Base/src/main/java/com/mike/base/utils/ImmersionBarUtil.java
@@ -0,0 +1,29 @@
+package com.mike.base.utils;
+
+import android.app.Activity;
+import android.view.View;
+import com.gyf.immersionbar.ImmersionBar;
+import com.mike.base.R;
+
+/**
+ * created by  wjf  at 2020/3/24 17:22
+ */
+public class ImmersionBarUtil {
+
+    public static void activityBar(Activity activity) {
+        ImmersionBar.with(activity)
+                .statusBarColor(R.color.pink)     //状态栏颜色，不写默认透明色
+                .fitsSystemWindows(true)    //解决状态栏和布局重叠问题，任选其一，默认为false，当为true时一定要指定statusBarColor()，不然状态栏为透明色，还有一些重载方法
+                .init();  //必须调用方可应用以上所配置的参数
+    }
+
+    public static void activityImmersionBar(Activity activity) {
+        ImmersionBar.with(activity).init();
+    }
+
+    public static void activityImmersionBar(Activity activity, View view) {
+        ImmersionBar.with(activity)
+                .statusBarView(view)
+                .init();
+    }
+}
---
+++ Base/src/main/java/com/mike/base/utils/ToastUtil.java
@@ -0,0 +1,31 @@
+package com.mike.base.utils;
+
+import android.graphics.Color;
+import android.text.TextUtils;
+import com.blankj.utilcode.util.ToastUtils;
+
+/** created by  wjf  at 2021/8/24 14:42 */
+public class ToastUtil {
+
+    public static void show(String msg) {
+
+        //setGravity     : 设置吐司位置
+        //setBgColor     : 设置背景颜色
+        //setBgResource  : 设置背景资源
+        //setMsgColor    : 设置消息颜色
+        //setMsgTextSize : 设置消息字体大小
+        //showShort      : 显示短时吐司
+        //showLong       : 显示长时吐司
+        //showCustomShort: 显示短时自定义吐司
+        //showCustomLong : 显示长时自定义吐司
+        //cancel         : 取消吐司显示
+
+        if (!TextUtils.isEmpty(msg)) {
+            ToastUtils.make()
+                    .setBgColor(Color.MAGENTA)
+                    .setTextColor(Color.WHITE)
+                    .setTextSize(18)
+                    .show(msg);
+        }
+    }
+}
